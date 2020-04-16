4398495cebcdd41a922bc6e0f2595037b582f0c8
kfir2037 <34978872+kfir2037@users.noreply.github.com>
2020-04-16 20:02:27 +0300

    watch tasks screen

--- screens/swScreens/WatchFamiliesTasks.js
+++ screens/swScreens/WatchFamiliesTasks.js
@@ -1,10 +1,19 @@
 
 
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View,YellowBox, Image } from 'react-native';
+import { Platform, StyleSheet, Text, View,YellowBox, Image,ActivityIndicator } from 'react-native';
 import DateTime from 'react-native-customize-selected-date'
 import _ from 'lodash'
 import firebase from '../../config/config';
+import { ScrollView } from 'react-native-gesture-handler';
+
+YellowBox.ignoreWarnings(['Setting a timer']);
+const _console = _.clone(console);
+console.warn = message => {
+  if (message.indexOf('Setting a timer') <= -1) {
+    _console.warn(message);
+  }
+};
 
 export default class App extends Component {
   constructor(props) {
@@ -23,15 +32,19 @@ export default class App extends Component {
     let tasks = await this.getTasks();
 
   }
+  toTimestamp2(year,month,day){
+    // var datum = new Date(Date.UTC(year.toString(),(month-1).toString(),day.toString()));
+    console.log('year: ',year)
+    console.log('month: ',month)
+    console.log('day: ',day)
+        var datum = new Date(Date.UTC('2020','04','18'));
+    console.log('datum: ',datum)
+    return datum.getTime()/1000;
+   }
 
   getTasks = async () => {
     YellowBox.ignoreWarnings(['Setting a timer']);
-
-    // let allFamilies = []
-    // let familyObj = {}
     const familyId = this.props.navigation.state.params.familyId;
-    // console.log("familyuId: ",familyId);
-
 
     let morningTasks = firebase.firestore().collection('RoutineTasks').doc('morning');
 
@@ -94,26 +107,26 @@ export default class App extends Component {
               console.log('Error getting document', err);
             });
 
-            const swFamilies = await firebase
-            .firestore()
-            .collection('tasks')
-            .where('family_id', '==', familyId) 
-            .get()
-            .then(querySnapshot => {
-              querySnapshot.forEach(doc => {
-                // console.log('doc: ',doc._document.key.path.segments[6]);
-                let state = this.state.customTasks;
-                state.push(doc._document)
-                this.setState({customTasks:state})
-                // console.log('custom tasks arr: ',this.state.customTasks);
+            // const swFamilies = await firebase
+            // .firestore()
+            // .collection('tasks')
+            // .where('family_id', '==', familyId) 
+            // .get()
+            // .then(querySnapshot => {
+            //   querySnapshot.forEach(doc => {
+            //     // console.log('doc: ',doc._document.key.path.segments[6]);
+            //     let state = this.state.customTasks;
+            //     state.push(doc._document)
+            //     this.setState({customTasks:state})
+            //     // console.log('custom tasks arr: ',this.state.customTasks);
 
-              });
-              // this.setState({ data: allFamilies });
-              // console.log('data: ', this.state.data);
-            })
-            .catch(error => {
-              console.log("Error getting documents: ", error);
-            });
+            //   });
+            //   // this.setState({ data: allFamilies });
+            //   // console.log('data: ', this.state.data);
+            // })
+            // .catch(error => {
+            //   console.log("Error getting documents: ", error);
+            // });
 
 
 
@@ -137,14 +150,53 @@ export default class App extends Component {
 
     return familyId;
   }
-  onChangeDate(date) {
-    let arr=[]
-    for(let i=0;i<10;i++){
-      arr.push(date)
-    }
-    this.setState({test:arr})
-    
-    alert(date)
+  async onChangeDate(date) {
+    let chosenDayTasks=[];
+
+    this.setState({customTasks:chosenDayTasks})
+    console.log('date: ',typeof date);
+    let chooseDay = date[8]+date[9];
+    let chooseMonth = date[5]+date[6];
+    let chooseYear = date[0]+date[1]+date[2]+date[3];
+    // let chooseDateTimestamp = this.toTimestamp2(chooseYear,chooseMonth,chooseDay);
+    let taskDay = '';
+    let taskMonth = '';
+    let taskYear = '';
+
+    const familyId = this.props.navigation.state.params.familyId;
+
+    const swFamilies = await firebase
+    .firestore()
+    .collection('tasks')
+    .where('family_id', '==', familyId)  
+    .get()
+    .then(querySnapshot => {
+      querySnapshot.forEach(doc => {
+        // let date = this.state.customTasks;
+        // let state = this.state.customTasks;
+        let date = doc._document.proto.fields.date.timestampValue;
+        console.log('doc._document: ',doc._document.proto.fields.date.timestampValue)
+        taskDay = date[8]+date[9];
+        taskMonth = date[5]+date[6];
+        taskYear = date[0]+date[1]+date[2]+date[3];
+        if(taskDay==chooseDay&&taskMonth==chooseMonth&&taskYear==chooseYear){
+          // chosenDayTasks.push(doc._document.proto)
+          let state = this.state.customTasks;
+          state.push(doc._document.proto)
+          this.setState({customTasks:state})
+        }
+        // state.push(doc._document)
+        // this.setState({customTasks:state})
+
+      });
+      console.log('chosenDayTasks: ',chosenDayTasks)
+      console.log(taskDay+taskMonth+taskYear)
+      // let taskTimeStamp = this.toTimestamp2(taskYear,taskMonth,taskDay);
+      // console.log('timestamp2: ',taskTimeStamp);
+    })
+    .catch(error => {
+      console.log("Error getting documents: ", error);
+    });
 
   }
 
@@ -158,13 +210,19 @@ export default class App extends Component {
   }
 
   render() {
-   
-    let items = this.state.customTasks.map((item,key)=>
-      JSON.stringify(item.proto.fields.test)
-    );
-    items = items.map((item,key)=>
-      console.log(item)
-    );
+   let cusTasks=[];
+  //  if (cusTasks.length<1) {
+  //   return <ActivityIndicator  />;
+  // }
+    let items = this.state.customTasks.map(function(item){
+    cusTasks.push(item.fields.title.stringValue),
+    console.log('timestamp: ',item.fields.date.timestampValue)
+
+    });
+    // let arr = items.map(function(item){
+    //   console.log('3: ',item.proto.fields.test)
+    // });
+    console.log('cusTasks2: ', cusTasks)
     // let items = this.state.customTasks.map((item,key)=>
     // <Text key='1'>{item.proto.fields.test}</Text>
     // )
@@ -178,19 +236,20 @@ export default class App extends Component {
             renderChildDay={(day) => this.renderChildDay(day)}
           />
         </View>
+        {/* <ScrollView> */}
         <View style={styles.tasks}>    
-        <Text>משימות בוקר</Text>      
-         <Text>{this.state.morningTasks}</Text>
-         <Text>משימות צהריים</Text>      
-         <Text>{this.state.noonTasks}</Text>
-         <Text>משימות אחר הצהריים</Text>      
-         <Text>{this.state.afternoonTasks}</Text>
-         <Text>משימות ערב</Text>      
-         <Text>{this.state.eveningTasks}</Text>
-         <Text>משימות מותאמות</Text>      
-        {/* <Text>{items}</Text> */}
+        {/* <Text style={styles.tasksTitle}>משימות בוקר</Text>      
+         <Text style={styles.tasksList}>{this.state.morningTasks}</Text>
+         <Text style={styles.tasksTitle}>משימות צהריים</Text>      
+         <Text style={styles.tasksList}>{this.state.noonTasks}</Text>
+         <Text style={styles.tasksTitle}>משימות אחר הצהריים</Text>      
+         <Text style={styles.tasksList}>{this.state.afternoonTasks}</Text>
+         <Text style={styles.tasksTitle}>משימות ערב</Text>      
+         <Text style={styles.tasksList}>{this.state.eveningTasks}</Text> */}
+         <Text style={styles.tasksTitle}>משימות מותאמות</Text>      
+        <Text style={styles.tasksList}>{cusTasks}</Text>
         </View>
-        
+        {/* </ScrollView> */}
       </View>
     );
   }
@@ -211,6 +270,11 @@ const styles = StyleSheet.create({
     flex:3,
 
     
+  },
+  tasksTitle:{
+    fontSize:20,
+    color:'red',
+    fontWeight:'bold'
   },
   icLockRed: {
     width: 13 / 2,
